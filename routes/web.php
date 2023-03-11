9880c7a76c3c8f607a8db2534ab627ee79c242fd
TheSowut <sowutmail@gmail.com>
2023-03-11 13:07:58 +0200

    chore: debugging & query params

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -18,15 +19,28 @@ Route::get('/', function () {
 });
 
 Route::get('/hello', function () {
-   return response('<h1>Hello World</h1>', 404)
+    return response('<h1>Hello World</h1>', 404)
 //     will not render the html, due to text/plain header
-       ->header('Content-Type', 'text/plain')
-       ->header('foo', 'bar');
+        ->header('Content-Type', 'text/plain')
+        ->header('foo', 'bar');
 });
 
 // Wildcard route
-Route::get('/posts/{id}', function($id) {
+Route::get('/posts/{id}', function ($id) {
     return response('Post ' . $id);
 })
 // Match wildcard number only
-->where('id', '[0-9]+');
+    ->where('id', '[0-9]+');
+
+// Debugging
+Route::get('/debug/{id}', function ($id) {
+//     Pauses and displays the property.
+    dd($id);
+//     ddd() for more info.
+    return response('The id is ' . $id);
+});
+
+Route::get('/search', function (Request $req) {
+    return response('name: ' . $req->name . ',<br> city: ' . $req->city);
+//    http://localhost:8000/search?name=123&city=456
+});
