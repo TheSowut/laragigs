2627698d7c7606b4c6a3911b76c6f2f2fd41b9e0
TheSowut <sowutmail@gmail.com>
2023-03-12 19:52:56 +0200

    style: root page listing

--- resources/views/listing.blade.php
+++ resources/views/listing.blade.php
@@ -1,2 +1,9 @@
-<h1>{{$listing['title']}}</h1>
-<p>{{$listing['description']}}</p>
+@extends('layout')
+
+@section('content')
+    @include('partials._search')
+
+    <h1>{{$listing['title']}}</h1>
+    <p>{{$listing['description']}}</p>
+
+@endsection
